9db1893fac6a26b3ff3c7823cf601c5c0a9535df
Your Name <alanmayer@email.arizona.edu>
2020-10-09 17:39:35 -0700

    types

--- Sscript.js
+++ Sscript.js
@@ -1,5 +1,65 @@
-//generate random password
-function generate() {
+// Dom elements
+const resultEL = document.getElementById('result');
+const lengthEL = document.getElementById('length');
+const uppercaseEL = document.getElementById('uppercase');
+const lowercaseEL = document.getElementById('lowercase');
+const numbersEL = document.getElementById('numbers');
+const symbolsEL = document.getElementById('symbols');
+const generateEL = document.getElementById('generate');
 
-    //set password length
+const randomFunction = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNumber,
+    Symbol: getRandomSymbol
+};
+
+// Generate event lister
+generateEL.addEventListener('click', () => {
+    const length = +lengthEL.value;
+    const hasLower = lowercaseEL.checked;
+    const hasUpper = uppercaseEL.checked;
+    const hasNumber = numbersEL.checked;
+    const hasSymbol = symbolsEL.checked;
+
+    resultEL.innerText = generatePassword(
+        hasLower,
+        hasUpper,
+        hasNumber,
+        hasSymbol,
+        length
+    );
+});
+
+//Generate Password Function
+function generatePassword(lower, upper, number, symbol, length) {
+    // 1. Initialize password variable
+    // 2. Filter out unchecked types
+    // 3. Loop over the length. Then call a generator function for each type
+    // 4. Add final password to the password variable and return
+
+    let generatePassword = '';
+
+    const typesCount = lower + upper + number + symbol;
+
+    console.log('typesCount: ', typesCount);
+}
+
+//generate random password
+
+function getRandomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+    const symbols = '!@#$%^&*(){}[]=<>/,.';
+    return symbols[Math.floor(Math.random() * symbols.length)];
 }
